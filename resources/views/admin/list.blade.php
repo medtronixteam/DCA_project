8390b7ff713d26c0fcfe2a58b07deb3303aa714a
zeeshanali90 <158147792+zeeshanali90@users.noreply.github.com>
2024-10-16 21:48:30 -0700

    comit shan

--- resources/views/admin/list.blade.php
+++ resources/views/admin/list.blade.php
@@ -1,40 +1,40 @@
 @extends('layouts.admin')
 @section('content')
-<link rel="stylesheet" href="{{ url('admin/assets/css/font-awesome-n.min.css') }}">
-<link rel="stylesheet" href="{{ url('admin/assets/css/widget.css') }}">
+    <link rel="stylesheet" href="{{ url('admin/assets/css/font-awesome-n.min.css') }}">
+    <link rel="stylesheet" href="{{ url('admin/assets/css/widget.css') }}">
 
-<div class="pcoded-content">
+    <div class="pcoded-content">
 
-   <div class="row p-lg-5 p-2">
-    <div class="col-12 col-lg-12 col-md-12 py-2 p-2 ms-lg-2">
-        <div class="card shadow-sm">
-            <div class="card-header bg-black text-white">
-                <h1 class="mb-0">User List</h1>
-            </div>
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-bordered table-hover">
-                        <thead class="thead-light">
-                            <tr>
-                                <th>#</th>
-                                <th>User Name</th>
-                                <th>Email</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($users as $user)
-                                <tr>
-                                    <td>{{ $user->id }}</td>
-                                    <td>{{ $user->name }}</td>
-                                    <td>{{ $user->email }}</td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+        <div class="row p-lg-5 p-2">
+            <div class="col-12 col-lg-12 col-md-12 py-2 p-2 ms-lg-2">
+                <div class="card shadow-sm">
+                    <div class="card-header bg-black text-white">
+                        <h1 class="mb-0">User List</h1>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-hover">
+                                <thead class="thead-light">
+                                    <tr>
+                                        <th>#</th>
+                                        <th>User Name</th>
+                                        <th>Email</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($users as $user)
+                                        <tr>
+                                            <td>{{ $user->id }}</td>
+                                            <td>{{ $user->name }}</td>
+                                            <td>{{ $user->email }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-   </div>
-</div>
 @endsection
--- resources/views/admin/plan.blade.php
+++ resources/views/admin/plan.blade.php
@@ -1,123 +1,126 @@
 @extends('layouts.admin')
 @section('content')
+    <style>
+        .card-title {
+            font-size: 1.2rem;
+            font-weight: 600;
+        }
 
-<style>
-    .card-title {
-        font-size: 1.2rem;
-        font-weight: 600;
-    }
+        .card h2 {
+            color: #333;
+        }
 
-    .card h2 {
-        color: #333;
-    }
+        .card button {
+            width: 100%;
+            padding: 10px;
+        }
 
-    .card button {
-        width: 100%;
-        padding: 10px;
-    }
+        .card ul {
+            padding-left: 0;
+        }
 
-    .card ul {
-        padding-left: 0;
-    }
+        .card li {
+            font-size: 1rem;
+            margin-bottom: 0.5rem;
+        }
 
-    .card li {
-        font-size: 1rem;
-        margin-bottom: 0.5rem;
-    }
+        .badge {
+            font-size: 0.8rem;
+            padding: 0.3rem;
+        }
 
-    .badge {
-        font-size: 0.8rem;
-        padding: 0.3rem;
-    }
+        .custom {
+            border: 2px solid black;
+        }
 
-    .custom {
-        border: 2px solid black;
-    }
+        .custom:hover {
+            color: white;
+            background-color: #737988;
+        }
+    </style>
 
-    .custom:hover {
-        color: white;
-        background-color: #737988;
-    }
-</style>
+    <link rel="stylesheet" href="{{ url('admin/assets/css/font-awesome-n.min.css') }}">
+    <link rel="stylesheet" href="{{ url('admin/assets/css/widget.css') }}">
 
-<link rel="stylesheet" href="{{ url('admin/assets/css/font-awesome-n.min.css') }}">
-<link rel="stylesheet" href="{{ url('admin/assets/css/widget.css') }}">
+    <div class="pcoded-content">
+        <div class="page-header card">
+            <h1>Plans</h1>
+        </div>
 
-<div class="pcoded-content">
-    <div class="page-header card">
-        <h1>Plans</h1>
-    </div>
-
-    <div class="pcoded-inner-content">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-lg-4 col-md-6">
-                    <div class="card p-4 rounded" style="height:285px; background-color:rgb(14, 13, 13); color:white;">
-                        <h5 class="card-title">Free</h5>
-                        <h3 class="fw-bold mb-2">$0/month</h3>
-                        <p class="">Free of cost</p>
-                        <button class="btn custom text-white rounded-2" style="margin-top: 65px; border: 1px solid #737988">Active Plan</button>
+        <div class="pcoded-inner-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-lg-4 col-md-6">
+                        <div class="card p-4 rounded" style="height:285px; background-color:rgb(14, 13, 13); color:white;">
+                            <h5 class="card-title">Free</h5>
+                            <h3 class="fw-bold mb-2">$0/month</h3>
+                            <p class="">Free of cost</p>
+                            <button class="btn custom text-white rounded-2"
+                                style="margin-top: 65px; border: 1px solid #737988">Active Plan</button>
+                        </div>
                     </div>
-                </div>
-                <div class="col-12 col-lg-4 col-md-6">
-                    <div class="card p-4 rounded">
-                        <h5 class="card-title">Pro</h5>
-                        <h3 class="fw-bold mb-2">${{ $plans[0]->price }}/{{ $plans[0]->duration }}</h3>
-                        <p class="text-muted">Save $144 with annual payment</p>
-                        <form action="" method="post" id="change-price-form">
-                            @csrf
-                            <input type="hidden" value="pro" name="name">
-                            <input type="hidden" value="month" name="duration">
-                            <input name="price" class="p-1 my-3 rounded-2" type="number" placeholder="Change price here" id="pro-price-1" data-plan-id="1" required>
-                            <button type="submit" class="btn mb-3 custom change-price-button rounded-2">Change Price</button>
-                        </form>
+                    <div class="col-12 col-lg-4 col-md-6">
+                        <div class="card p-4 rounded">
+                            <h5 class="card-title">Pro</h5>
+                            <h3 class="fw-bold mb-2">${{ $plans[0]->price }}/{{ $plans[0]->duration }}</h3>
+                            <p class="text-muted">Save $144 with annual payment</p>
+                            <form action="" method="post" id="change-price-form">
+                                @csrf
+                                <input type="hidden" value="pro" name="name">
+                                <input type="hidden" value="month" name="duration">
+                                <input name="price" class="p-1 my-3 rounded-2" type="number"
+                                    placeholder="Change price here" id="pro-price-1" data-plan-id="1" required>
+                                <button type="submit" class="btn mb-3 custom change-price-button rounded-2">Change
+                                    Price</button>
+                            </form>
+                        </div>
                     </div>
-                </div>
-                <div class="col-12 col-lg-4 col-md-6">
-                    <div class="card p-4 rounded">
-                        <h5 class="card-title">Expert</h5>
-                        <h3 class="fw-bold mb-2">${{ $plans[1]->price }}/{{ $plans[1]->duration }}</h3>
-                        <p class="text-muted">Save $240 with annual payment</p>
-                        <form action="" method="post" id="change-price-form2">
-                            @csrf
-                            <input type="hidden" value="expert" name="name">
-                            <input type="hidden" value="month" name="duration">
-                            <input name="price" class="p-1 my-3 rounded-2" type="number" placeholder="Change price here" id="expert-price" data-plan-id="2" required>
-                            <button type="submit" class="btn mb-3 custom change-price-button rounded-2">Change Price</button>
-                        </form>
+                    <div class="col-12 col-lg-4 col-md-6">
+                        <div class="card p-4 rounded">
+                            <h5 class="card-title">Expert</h5>
+                            <h3 class="fw-bold mb-2">${{ $plans[1]->price }}/{{ $plans[1]->duration }}</h3>
+                            <p class="text-muted">Save $240 with annual payment</p>
+                            <form action="" method="post" id="change-price-form2">
+                                @csrf
+                                <input type="hidden" value="expert" name="name">
+                                <input type="hidden" value="month" name="duration">
+                                <input name="price" class="p-1 my-3 rounded-2" type="number"
+                                    placeholder="Change price here" id="expert-price" data-plan-id="2" required>
+                                <button type="submit" class="btn mb-3 custom change-price-button rounded-2">Change
+                                    Price</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-    $(document).ready(function () {
-        function handleFormSubmission(formId) {
-            $(formId).submit(function (e) {
-                e.preventDefault();
-                $.ajax({
-                    type: 'POST',
-                    url: "{{ route('admin.plans.change-price') }}",
-                    data: new FormData(this),
-                    contentType: false,
-                    cache: false,
-                    processData: false,
-                    dataType: 'json',
-                    success: function (response) {
-                        notifyToast('Price updated successfully', 'inverse');
-                    },
-                    error: function (xhr) {
-                        notifyToast('An error occurred while updating', 'inverse');
-                    }
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            function handleFormSubmission(formId) {
+                $(formId).submit(function(e) {
+                    e.preventDefault();
+                    $.ajax({
+                        type: 'POST',
+                        url: "{{ route('admin.plans.change-price') }}",
+                        data: new FormData(this),
+                        contentType: false,
+                        cache: false,
+                        processData: false,
+                        dataType: 'json',
+                        success: function(response) {
+                            notifyToast('Price updated successfully', 'inverse');
+                        },
+                        error: function(xhr) {
+                            notifyToast('An error occurred while updating', 'inverse');
+                        }
+                    });
                 });
-            });
-        }
-        handleFormSubmission('#change-price-form');
-        handleFormSubmission('#change-price-form2');
-    });
-</script>
-
+            }
+            handleFormSubmission('#change-price-form');
+            handleFormSubmission('#change-price-form2');
+        });
+    </script>
 @endsection
